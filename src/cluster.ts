aef0f3822775f7a03530a215ae4ab7f6f06d0a8f
YauheniBeiduk <Yauheni_Beiduk@epam.com>
2023-01-09 14:33:39 +0400

    feat: update ReadME

--- src/cluster.ts
+++ src/cluster.ts
@@ -1,21 +1,25 @@
-import cluster from 'node:cluster';
-import { cpus } from 'node:os';
-import process from 'node:process';
+import cluster from 'cluster';
+import { cpus } from 'os';
+import process from 'process';
 
+const port = process.env.PORT;
+
+const workers: any = [];
 
 if (cluster.isPrimary) {
     console.log(`Master ${process.pid} is running`);
     const numCPUs = cpus().length;
 
     for (let i = 0; i < numCPUs; i++) {
-        cluster.fork({PORT: 4000 + i});
+        const worker = cluster.fork({ PORT: 4000 + i + 1 });
+        workers.push(worker);
+    }
 
 
         cluster.on("message", (worker, message) => {
             console.log(message);
             worker.send(message);
         })
-    }
-} else {
-    console.log(process.env.PORT)
+    } else {
+    console.log(port);
 }
